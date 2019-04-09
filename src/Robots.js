53d81358b7ba437ba1674774762a597e400ad89d
ermancete <ermancete@gmail.com>
2019-04-09 18:17:47 +0300

    first commit

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const Robots = [
+{
+	id: 1,
+	name: 'erman cete',
+	username: 'ermancete',
+	email: 'ermancete@gmail.com'
+},
+{
+	id: 2,
+	name: 'bulut topuz',
+	username: 'yubuto',
+	email: 'yunusbuluttopuz@gmail.com'
+},
+{
+	id: 3,
+	name: 'gorkem demirok',
+	username: 'gorkdeu',
+	email: 'gorkd@gmail.com'
+},
+{
+	id: 4,
+	name: 'asdsd cete',
+	username: 'ersdfsfmancete',
+	email: 'dfa@gmail.com'
+},
+{
+	id: 5,
+	name: 'weqr cete',
+	username: 'ermancete',
+	email: 'ermancete@gmail.com'
+},
+{
+	id: 6,
+	name: 'mhön cete',
+	username: 'ermancete',
+	email: 'ermancete@gmail.com'
+},
+{
+	id: 7,
+	name: 'xvcb cete',
+	username: 'ermancete',
+	email: 'ermancete@gmail.com'
+},
+{
+	id: 8,
+	name: 'fgd cete',
+	username: 'ermancete',
+	email: 'ermancete@gmail.com'
+},
+{
+	id: 9,
+	name: 'erreman cete',
+	username: 'ermancete',
+	email: 'ermancete@gmail.com'
+},
+{
+	id: 10,
+	name: 'erman cetelasdkask',
+	username: 'kasdkask',
+	email: 'ermancete@jasdkas.com'
+},
+]
